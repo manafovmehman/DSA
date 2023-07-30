c864922eea194517351137e430396c844651ca08
Mehman Manafov <44061428+manafovmehman@users.noreply.github.com>
2023-07-30 08:02:01 -0400

    Time: 24 ms (99.76%), Space: 50.7 MB (69.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,20 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i)
-            if (dups.add(nums[i])) {
-                for (int j = i + 1; j < nums.length; ++j) {
-                    int complement = -nums[i] - nums[j];
-                    if (seen.containsKey(complement) && seen.get(complement) == i) {
-                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                        Collections.sort(triplet);
-                        res.add(triplet);
-                    }
-                    seen.put(nums[j], i);
-                }
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumII(nums, i, res);
             }
-        return new ArrayList(res);
+        return res;
+    }
+    void twoSumII(int[] nums, int i, List<List<Integer>> res) {
+        int lo = i + 1, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                lo++;
+                hi--;
+                while (lo < hi && nums[lo] == nums[lo - 1])
+                    lo++;
+                while (lo < hi && nums[hi] == nums[hi + 1])
+                    hi--;
+            }
+        }
     }
 }
